293d1df330e2c161cd1b561ca8c7752aaffc4e86
P-ski <p.ski1@icloud.com>
2025-08-19 16:43:27 +0200

    uno

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+const DATA = [
+  { id: "todo-0", name: "Eat", completed: true },
+  { id: "todo-1", name: "Sleep", completed: false },
+  { id: "todo-2", name: "Repeat", completed: false },
+];
+
+console.log(DATA)
+
+
+
+ createRoot(document.getElementById('root')).render(
+   <StrictMode>
+     <App tasks={DATA}/>
+  </StrictMode>,
+)
